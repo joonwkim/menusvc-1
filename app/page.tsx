086fd0844895da9c69ffbe33c18e4bc1814121f2
jwk <joonwk@gmail.com>
2024-08-07 21:27:05 +0900

    init

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <h2>Menu Service</h2>
+      </div>
+    </main>
+  );
+}
